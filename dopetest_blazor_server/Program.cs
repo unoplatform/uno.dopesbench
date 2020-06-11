5ab61d60c7b19cfa9e40ed7f4a14f77f79eb5afd
David Oliver <david.oliver@nventive.com>
2020-06-11 14:21:43 -0400

    Add Dope rate sample to Blazor server head

--- dopetest_blazor_server/Program.cs
+++ dopetest_blazor_server/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace dopetest_blazor_server
+namespace dopetest_blazor
 {
 	public class Program
 	{
--- dopetest_blazor_server/Startup.cs
+++ dopetest_blazor_server/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace dopetest_blazor_server
+namespace dopetest_blazor
 {
 	public class Startup
 	{
